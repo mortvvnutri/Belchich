36abf1223928cdf56caee64f0a89301ec9fb86f5
mortvvnutri <93926034+mortvvnutri@users.noreply.github.com>
2022-12-20 21:53:00 +0300

    Add files via upload

---
+++ PZ_5/Book.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_5
+{
+    internal class Book
+    {
+        public int id;
+        public string author;
+        public string name;
+        public int year;
+        public string made;
+        public int str;
+
+        // Конструктор
+
+        public Book()
+        { }
+
+        public Book(int id, string author, string name, int year, string made, int str)
+        {
+            this.id = id;
+            this.author = author;
+            this.name = name;
+            this.year = year;
+            this.made = made;
+            this.str = str;
+        }
+
+        public Book(int id, string author, string name, int year, string made)
+        {
+            this.id = id;
+            this.author = author;
+            this.name = name;
+            this.year = year;
+            this.made = made;
+        }
+
+        // Методы
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine("Регистрационный номер:         {0}", id);
+            Console.WriteLine("Автор:         {0}", author);
+            Console.WriteLine("Название:    {0}", name);
+            Console.WriteLine("Год издания:    {0}", year);
+            Console.WriteLine("Издательство:    {0}", made);
+            Console.WriteLine("Кол-во страниц:    {0}", str);
+
+            string s = Console.ReadLine();
+        }
+
+    }
+}
---
+++ PZ_5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PZ_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book emp1 = new Book();
+
+            emp1.id = 1;
+            emp1.author = "Кристина Старк";
+            emp1.name = "Стигмарион";
+            emp1.year = 2018;
+            emp1.made = "АСТ";
+            emp1.str = 420;
+            emp1.DisplayStatus();
+
+
+            Book emp2 = new Book(2, "Дж. К. Роулинг", "Гарри Поттер и Дары смерти", 2007, "Росмэн", 605);
+            emp2.DisplayStatus();
+
+            Book emp3 = new Book(3, "Дж. К. Роулинг", "Гарри Поттер и Дары смерти 2", 2009, "Росмэн");
+            emp3.DisplayStatus();
+
+        }
+    }
+}
